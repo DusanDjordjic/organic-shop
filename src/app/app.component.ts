985dc9f991d50ab48d9e8602011c952cffaf6a75
Dusan Djordjic <77781108+DusanDjordjic@users.noreply.github.com>
2021-10-27 23:11:33 +0200

    about and details section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductInterface } from './Interfaces/product.interface';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  prodctsArray: ProductInterface[] = [];
   searchQuery: string = '';
+  activeProduct: ProductInterface;
+  constructor(private productService: ProductService) {
+    this.prodctsArray = productService.getProducts();
+    this.activeProduct = this.prodctsArray[0];
+    console.log(this.activeProduct);
+  }
   onSearchSubmit() {
     console.log('serach submit');
+  }
+  setActiveDetail(name: string) {
+    this.activeProduct = this.prodctsArray.filter(
+      (product) => product.name === name
+    )[0];
   }
 }
